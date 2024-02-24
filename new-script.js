fd0f9865278ac212e8d3d632d02e064ee987fdd7
Erry Azhari <eraya.ashi@gmail.com>
2024-02-24 18:39:26 +0700

    refactor js 1

--- new-script.js
+++ new-script.js
@@ -1,8 +1,51 @@
 var navigator = window.navigator;
+var isPopUpShown = [false, false, false, false, false];
+var isShownAlert = false;
 
-function onBlow(target) {
+
+function alertOnce() {
+    if (!isShownAlert) {
+      const defaults = {
+        spread: 360,
+        ticks: 100,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["heart"],
+        colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+      };
+      
+      confetti({
+        ...defaults,
+        particleCount: 50,
+        scalar: 2,
+      });
+      
+      confetti({
+        ...defaults,
+        particleCount: 25,
+        scalar: 3,
+      });
+      
+      confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 4,
+      });
+  
+      isShownAlert = true;
+    }
+  
+  }
+  
+  function showPopUp(target, index) {
     target.style.display = "none";
-}
+    isPopUpShown[index] = true;
+  
+    if (isPopUpShown[0] && isPopUpShown[1] && isPopUpShown[2] && isPopUpShown[3] && isPopUpShown[4]) {
+      alertOnce();
+    }
+  }
 
 navigator.mediaDevices.getUserMedia({ audio: true })
   .then(stream => {
@@ -31,6 +74,8 @@ navigator.mediaDevices.getUserMedia({ audio: true })
         shapes: ["heart"],
         colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
     };
+
+    var flameTimer = 0;
 
     const intervalId = setInterval(() => {
       analyser.getByteTimeDomainData(frequencyData);
