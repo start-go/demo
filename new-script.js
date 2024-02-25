60be29e78df39d1c260bb748e2bef367557e779d
Erry Azhari <eraya.ashi@gmail.com>
2024-02-25 12:13:56 +0700

    configurable

--- new-script.js
+++ new-script.js
@@ -1,7 +1,18 @@
-var navigator = window.navigator;
-var isPopUpShown = [false, false, false, false, false];
-var isShownAlert = false;
+let isPopUpShown = [false, false, false, false, false];
+let isShownAlert = false;
 
+// init browser requirement
+const navigator = window.navigator;
+const queryString = window.location.search;
+
+// get params
+const urlParams = new URLSearchParams(queryString);
+const avgTiggered = Number(urlParams.get('trigger')) || 30;
+const message = String(urlParams.get('msg')) || "Default";
+const showLog = Boolean(urlParams.get('log')) || false;
+
+// init element
+const cake = document.getElementById("cake");
 const fire0 = document.getElementById("fire0");
 const fire1 = document.getElementById("fire1");
 const fire2 = document.getElementById("fire2");
@@ -9,7 +20,6 @@ const fire3 = document.getElementById("fire3");
 const fire4 = document.getElementById("fire4");
 const popText = document.getElementById("pop-text");
 const logger = document.getElementById("logger");
-const avgTiggered = 30;
 
 const defaults = {
     spread: 360,
@@ -19,8 +29,18 @@ const defaults = {
     startVelocity: 30,
     shapes: ["heart"],
     colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
-  };
+};
 
+
+function main() {
+    popText.innerText = message;
+
+    if (showLog) {
+        cake.style.display = "none";
+    }
+
+    captureMic();
+}
 
 function popConfetti(particle, scalar) {
     confetti({
@@ -36,79 +56,84 @@ function alertOnce() {
 
         setTimeout(function(){
             popConfetti(50, 2);
-            }, 500); 
-        
-            setTimeout(function(){
+        }, 500); 
+    
+        setTimeout(function(){
             popConfetti(25, 3);
-            }, 1500); 
-        
-            setTimeout(function(){
+        }, 1500); 
+    
+        setTimeout(function(){
             popConfetti(10, 4);
-            }, 2500); 
+        }, 2500); 
 
-        popText.style.display = "block";
-            
+        popText.style.display = "block";      
     }  
 }
   
-  function showPopUp(target, index) {
+function showPopUp(target, index) {
     target.style.display = "none";
     isPopUpShown[index] = true;
   
     if (isPopUpShown[0] && isPopUpShown[1] && isPopUpShown[2] && isPopUpShown[3] && isPopUpShown[4]) {
       alertOnce();
     }
-  }
+}
 
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(stream => {
-    const audioContext = new AudioContext();
-    const analyser = audioContext.createAnalyser();
-    const microphone = stream.getAudioTracks()[0];
-    const source = audioContext.createMediaStreamSource(stream);
-    source.connect(analyser);
+function captureMic() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        const audioContext = new AudioContext();
+        const analyser = audioContext.createAnalyser();
+        const microphone = stream.getAudioTracks()[0];
+        const source = audioContext.createMediaStreamSource(stream);
+        source.connect(analyser);
 
-    analyser.fftSize = 2048;
-    const bufferLength = analyser.frequencyBinCount;
-    const frequencyData = new Uint8Array(bufferLength);
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const frequencyData = new Uint8Array(bufferLength);
 
-    const defaults = {
-        spread: 360,
-        ticks: 100,
-        gravity: 0,
-        decay: 0.94,
-        startVelocity: 30,
-        shapes: ["heart"],
-        colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
-    };
+        const defaults = {
+            spread: 360,
+            ticks: 100,
+            gravity: 0,
+            decay: 0.94,
+            startVelocity: 30,
+            shapes: ["heart"],
+            colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+        };
 
-    var flameTimer = 0;
+        var flameTimer = 0;
 
-    const intervalId = setInterval(() => {
-      analyser.getByteFrequencyData(frequencyData);
-      const average = frequencyData.reduce((acc, val) => acc + val, 0) / frequencyData.length;
+        const intervalId = setInterval(() => {
+        analyser.getByteFrequencyData(frequencyData);
+        const average = frequencyData.reduce((acc, val) => acc + val, 0) / frequencyData.length;
 
-      logger.innerText = frequencyData.length + " | " + average
-    //   console.log(frequencyData.length, average)
+        if(showLog) {
+            logger.innerText = frequencyData.length + " | " + average + " >= " + avgTiggered
+        }
 
-      if (average >= avgTiggered) {
-        showPopUp(fire0, 0);
-        showPopUp(fire1, 1);
-        showPopUp(fire2, 2);
-        showPopUp(fire3, 3);
-        showPopUp(fire4, 4);
+        if (average >= avgTiggered) {
+            showPopUp(fire0, 0);
+            showPopUp(fire1, 1);
+            showPopUp(fire2, 2);
+            showPopUp(fire3, 3);
+            showPopUp(fire4, 4);
 
+            clearInterval(intervalId);
+            microphone.stop();
+        }
+        }, 100);
+
+        // Stop the analysis when the user closes the browser window/tab
+        window.addEventListener("beforeunload", () => {
         clearInterval(intervalId);
         microphone.stop();
-      }
-    }, 100);
-
-    // Stop the analysis when the user closes the browser window/tab
-    window.addEventListener("beforeunload", () => {
-      clearInterval(intervalId);
-      microphone.stop();
+        });
+    })
+    .catch(error => {
+        console.error("Error accessing microphone:", error);
     });
-  })
-  .catch(error => {
-    console.error("Error accessing microphone:", error);
-  });
+}
+
+
+main();
