a63cdae74d12927ef8357f89a6cf0f39ecec2221
Erry Azhari <eraya.ashi@gmail.com>
2024-02-25 13:20:28 +0700

    refactor css

--- icon.sh
+++ icon.sh
@@ -1,3 +1,5 @@
+# required imagemagick
+
 convert assets/icon/favicon.png -resize 16x16 -background none -extent 16x16 assets/icon/favicon-16x16.png
 convert assets/icon/favicon.png -resize 32x32 -background none -extent 32x32 assets/icon/favicon-32x32.png
 convert assets/icon/favicon.png -resize 180x180 -background none -extent 180x180 assets/icon/apple-touch-icon.png
